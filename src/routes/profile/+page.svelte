56e50bf4eb8273841b53be393a32666b02c4daad
sophmyn24 <139141656+sophmyn24@users.noreply.github.com>
2024-06-29 22:48:08 -0400

    text change

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<title>Profile</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Hi, name</h1>
+
+	<p>
+		Profile info:
+	</p>
+
+	<pre>username
+picture
+	</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
